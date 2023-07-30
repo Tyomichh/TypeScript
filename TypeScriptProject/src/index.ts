5ba873da9455a487f0beca197a9082a8541c2a96
Tyomichh <workemailaertemlozenko@gmail.com>
2023-07-30 20:09:35 +0300

    Config the project, sorted by folder

---
+++ TypeScriptProject/src/index.ts
@@ -0,0 +1,4 @@
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
